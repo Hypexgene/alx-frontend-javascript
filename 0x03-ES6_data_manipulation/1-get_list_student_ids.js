de25c0d0eb94ecb0f20ebbc5ce876f82e2c2f0c2
Hypexgene <ashtonmuiru@gmail.com>
2024-06-05 14:58:45 -0700

    0x04-TypeScript (+) 0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+[200~export default function getListStudentIds(studentList) {
+	  if (Array.isArray(studentList)) {
+		      const studentIds = studentList.map((student) => student.id);
+		      return studentIds;
+		    }
+	  return [];
+}
